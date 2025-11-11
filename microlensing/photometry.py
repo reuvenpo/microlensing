5130e7c42e29c0b5cc5a2913633803cd386ff629
Reuven Podmazo <reuven@enigma.co>
2025-11-11 18:48:44 +0200

    cleaned up PhotDat a little

--- microlensing/photometry.py
+++ microlensing/photometry.py
@@ -30,19 +30,19 @@ class PhotDat:
             see_est = df[3]
             sky_level = df[4]
 
-            # Convert Magnitude into Precentage Intensity - as per experiment instructions
+            # Convert Magnitude into Percentage Intensity - as per experiment instructions
             # Intensity of magnitude 0 for I-band based on https://irsa.ipac.caltech.edu/data/SPITZER/docs/dataanalysistools/tools/pet/magtojy/ref.html
             # Using Johnson scale I_0 gives us intensity in 1 Jy= 10−23 erg⋅s−1⋅cm−2⋅Hz−1
             i_0 = 2635
-            tens: NDFloatArray = np.full(shape=[I_mag.size], fill_value=10)
-            I_mag = I_mag / (-2.5)
-            I_mag_err = I_mag_err / (-2.5)
-            intensity = np.float_power(x1=tens, x2=I_mag) * i_0
-            intensity_err = np.float_power(x1=tens, x2=I_mag_err) * i_0
-            cls(hjd,intensity, intensity_err, see_est, sky_level)
+            tens: NDFloatArray = np.full_like(I_mag, 10)
+            intensity = tens ** (I_mag / -2.5) * i_0
+            intensity_err = tens ** (I_mag_err / -2.5) * i_0
+
+            cls(hjd, intensity, intensity_err, see_est, sky_level)
 
         except FileNotFoundError:
             print(f"File '{path}' not found.")
+            raise
         except Exception as e:
             print(f"An error occurred: {e}")
-
+            raise
