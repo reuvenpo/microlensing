4ad46d569298d1ddea569be629c03b5a6f8135d7
Reuven Podmazo <reuven@enigma.co>
2025-11-11 21:30:09 +0200

    fixed class PhotDat

--- microlensing/photometry.py
+++ microlensing/photometry.py
@@ -8,8 +8,18 @@ from numpy import ndarray, dtype
 from .loc_types import NDFloatArray
 
 
+def mag_to_i(mag):
+    """ Convert Magnitude into Percentage Intensity - as per experiment instructions
+    Intensity of magnitude 0 for I-band based on https://irsa.ipac.caltech.edu/data/SPITZER/docs/dataanalysistools/tools/pet/magtojy/ref.html
+    Using Johnson scale I_0 gives us intensity in 1 Jy= 10−23 erg⋅s−1⋅cm−2⋅Hz−1
+    """
+    i_0 = 2635
+    return np.full_like(mag, 10) ** (mag / -2.5) * i_0
+
+
 @dataclass()
 class PhotDat:
+    """Class representing the contents of phot.dat files from the OGLE experiment database"""
     # The five columns of the `phot.dat` files
     hjd: NDFloatArray
     intensity: NDFloatArray
@@ -19,26 +29,20 @@ class PhotDat:
 
     @classmethod
     def from_file(cls, path: str) -> "PhotDat":
-        """Initializing the data class with path, using example
-        from https://www.geeksforgeeks.org/python/reading-dat-file-in-python/"""
+        """Initializing the data class with path"""
         try:
             # Assuming data is in a structured format like CSV or similar
-            df = np.genfromtxt(path, delimiter=",")
-            hjd = df[0]
-            I_mag: NDFloatArray = df[1]
-            I_mag_err: NDFloatArray = df[2]
-            see_est = df[3]
-            sky_level = df[4]
+            df = np.genfromtxt(path, delimiter=" ")
+            hjd = df[..., 0]
+            I_mag: NDFloatArray = df[..., 1]
+            I_mag_err: NDFloatArray = df[..., 2]
+            see_est = df[..., 3]
+            sky_level = df[..., 4]
 
-            # Convert Magnitude into Percentage Intensity - as per experiment instructions
-            # Intensity of magnitude 0 for I-band based on https://irsa.ipac.caltech.edu/data/SPITZER/docs/dataanalysistools/tools/pet/magtojy/ref.html
-            # Using Johnson scale I_0 gives us intensity in 1 Jy= 10−23 erg⋅s−1⋅cm−2⋅Hz−1
-            i_0 = 2635
-            tens: NDFloatArray = np.full_like(I_mag, 10)
-            intensity = tens ** (I_mag / -2.5) * i_0
-            intensity_err = tens ** (I_mag_err / -2.5) * i_0
+            intensity = mag_to_i(I_mag)
+            intensity_err = mag_to_i(I_mag_err)
 
-            cls(hjd, intensity, intensity_err, see_est, sky_level)
+            return cls(hjd, intensity, intensity_err, see_est, sky_level)
 
         except FileNotFoundError:
             print(f"File '{path}' not found.")
