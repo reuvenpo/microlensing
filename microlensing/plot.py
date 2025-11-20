04530a41b3f480ab4559672f104217533100d2f6
Reuven Podmazo <reuven@enigma.co>
2025-11-20 21:28:33 +0200

    added a heatmap corner plot implementation and a test that creates an example plot

--- microlensing/plot.py
+++ microlensing/plot.py
@@ -5,6 +5,20 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import numpy as np
 
+from .loc_types import NDFloatArray
+
+# The chi squared difference as a function of confidence level and degrees of freedom.
+# Values are given for 68.3%, 90%, 95.4%, 99%, 99.73%, and 99.99%
+CHI2_DIFF_CONF_DOF = np.array([
+    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],  # filler
+    [0.00, 1.00, 2.71, 4.00, 6.63, 9.00, 15.1],  # 1 DoF
+    [0.00, 2.30, 4.61, 6.17, 9.21, 11.8, 18.4],  # 2 DoF
+    [0.00, 3.53, 6.25, 8.02, 11.3, 14.2, 21.1],  # 3 DoF
+    [0.00, 4.72, 7.78, 9.70, 13.3, 16.3, 23.5],  # 4 DoF
+    [0.00, 5.89, 9.24, 11.3, 15.1, 18.2, 25.7],  # 5 DoF
+    [0.00, 7.04, 11.6, 12.8, 16.8, 20.1, 27.8],  # 6 DoF
+])
+
 
 class Plot:
     """A helper class to quickly generate plots with reasonable defaults."""
@@ -38,26 +52,14 @@ class Plot:
         self.ax.errorbar(x=x, y=y, yerr=sigma, marker='o', linestyle='None')
         self.ax.plot(x, fit_y, style, label=label)
 
-    # The chi squared difference as a function of confidence level and degrees of freedom.
-    # Values are given for 68.3%, 90%, 95.4%, 99%, 99.73%, and 99.99%
-    CHI2_DIFF_CONF_DOF = np.array([
-        [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00], # filler
-        [0.00, 1.00, 2.71, 4.00, 6.63, 9.00, 15.1], # 1 DoF
-        [0.00, 2.30, 4.61, 6.17, 9.21, 11.8, 18.4], # 2 DoF
-        [0.00, 3.53, 6.25, 8.02, 11.3, 14.2, 21.1], # 3 DoF
-        [0.00, 4.72, 7.78, 9.70, 13.3, 16.3, 23.5], # 4 DoF
-        [0.00, 5.89, 9.24, 11.3, 15.1, 18.2, 25.7], # 5 DoF
-        [0.00, 7.04, 11.6, 12.8, 16.8, 20.1, 27.8], # 6 DoF
-    ])
-
     def plot_heatmap(self, x, y, z, z_label="z", num_levels=15, x_min=None, y_min=None, z_min=None):
         # Levels can be used to draw discreet levels based on chi2 confidence levels
         # levels = np.linspace(z.min(), z.max(), num_levels)
         X, Y = np.meshgrid(x, y)
         min = np.min(z)
-        levels = self.CHI2_DIFF_CONF_DOF[2] + min
+        levels = CHI2_DIFF_CONF_DOF[2] + min
         filled_contours = self.ax.contourf(X, Y, z, levels=levels)
-        # custom_norm = colors.Normalize(vmin=min, vmax=min+self.CHI2_DIFF_CONF_DOF[2][-1])
+        # custom_norm = colors.Normalize(vmin=min, vmax=min+CHI2_DIFF_CONF_DOF[2][-1])
         # self.ax.pcolormesh(z, norm=custom_norm)
         self.fig.colorbar(filled_contours, ax=self.ax, label=z_label)
         # cbar =
@@ -78,3 +80,79 @@ class Plot:
             filterwarnings("ignore")
             # self.ax.legend()
         self.fig.savefig(output_file)
+
+
+def heatmap_corner_plot(
+        data: NDFloatArray,
+        data_axes: list[NDFloatArray],
+        center: NDFloatArray | list[float],
+        levels: list[float],
+        title: str,
+        axis_names: list[str],
+):
+    """
+    :param data: The chunk of data for which a corner plot will be generated
+    :param data_axes: A list of arrays of values of parameters used along each axis to calculate `data`
+                 Must be organized in the same order as the axes of the `data` block
+    :param center: The index of the point in `data` which will be included in all plots
+    :return: figure, axes
+    """
+    dims = len(data.shape)
+
+    axis_combos = []
+    mosaic = []
+    for row in range(dims - 1):
+        mosaic_row = []
+        mosaic.append(mosaic_row)
+        for col in range(dims - 1):
+            if col > row:
+                mosaic_row.append(".")
+            else:
+                combo = (col, row+1)
+                combo_name = str(combo)
+                mosaic_row.append(combo_name)
+                axis_combos.append((combo_name, combo))
+        mosaic_row.append("colorbar")
+
+    fig_size = 8
+    fig, axes_dict = plt.subplot_mosaic(
+        mosaic,
+        width_ratios=[1]*(dims - 1) + [dims/20],
+        layout="compressed",
+        # figsize=(fig_size, fig_size),
+        dpi=300,
+    )
+    fig.suptitle(title)
+
+    x_axes = [None]*dims
+    y_axes = [None]*dims
+    contour_set = None  # Save one to use for the shared colorbar in the end
+    for combo_name, combo in axis_combos:
+        x, y = combo
+        axes = axes_dict[combo_name]
+        axes.set_xlabel(axis_names[x])
+        axes.set_ylabel(axis_names[y])
+        axes.set_aspect(1)
+        axes.label_outer(remove_inner_ticks=True)
+
+        # Share axes along rows and columns
+        shared_x_axis = x_axes[x]
+        if shared_x_axis is None:
+            x_axes[x] = axes
+        else:
+            axes.sharex(shared_x_axis)
+        shared_y_axis = y_axes[y]
+        if shared_y_axis is None:
+            y_axes[y] = axes
+        else:
+            axes.sharey(shared_y_axis)
+
+        contour_axes = [data_axes[axis] for axis in combo]
+        data_index = tuple(slice(None) if axis in combo else center[axis] for axis in range(dims))
+        contour_data = data[data_index]
+        contour_set = axes.contourf(*contour_axes, contour_data, levels=levels)
+        axes.contour(*contour_axes, contour_data, levels=levels, colors="black", linewidths=0.5)
+
+    fig.colorbar(contour_set, cax=axes_dict["colorbar"])
+
+    return fig, axes_dict
